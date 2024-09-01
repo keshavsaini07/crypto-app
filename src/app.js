83c9d628ceba0672d4fad3ab50272d858827ae74
keshavsaini07 <sainikeshav770@gmail.com>
2024-09-01 21:34:17 +0530

    test code 1

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
+app.get('/', (req, res) => res.send("Welcome to the app"));
 app.use('/api', apiRoutes);
 
 (async () => {
